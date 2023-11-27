3a9810445c46aa043230215cf11b0b759a8e58a8
unknown <f.carrara24@studenti.unibg.it>
2023-11-27 16:29:55 +0100

    results computed in response.php

--- response.php
+++ response.php
@@ -7,7 +7,12 @@ $wordToCensor = $_POST["wordToCensor"];
 
 //simple printing response
 echo $paragraph ."<br/>";
-echo $wordToCensor;
+//printing paragraph and its length
+$paragraphLength = strlen($paragraph);
+echo  "paragraph length: ".$paragraphLength."<br/>";
 
-
+// replacing all instances if world to censor in paragraph
+$paragraph = str_replace($wordToCensor, '***', $paragraph);
+echo "new paragraph: <br/>".$paragraph;
+echo "<br/>new paragraph length: ".$paragraphLength = strlen($paragraph);
 ?>
