5582b742d0ec78b3e5d7edec1741ed1d0c927645
unknown <f.carrara24@studenti.unibg.it>
2023-11-27 16:18:50 +0100

    communication between two parts

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>documento di partenza</title>
+</head>
+<body>
+    <form action="./response.php" method="post">
+        <label for="paragraph">Paragrafo con input da gestire</label> <br>
+        <textarea name="paragraph" id="paragraph" cols="30" rows="4"></textarea> 
 
+        <br><br>
 
-?>
+        <label for="wordtoCensor">Parola da censurare</label><br>
+        <input type="text" id="wordToCensor" name="wordToCensor">
+
+        <input type="submit" value="invia">
+    </form>
+</body>
+</html>
---
+++ response.php
@@ -0,0 +1,13 @@
+<h1>risposta</h1>
+
+<?php
+#creating variables to store all responses
+$paragraph = $_POST["paragraph"];
+$wordToCensor = $_POST["wordToCensor"];
+
+//simple printing response
+echo $paragraph ."<br/>";
+echo $wordToCensor;
+
+
+?>
